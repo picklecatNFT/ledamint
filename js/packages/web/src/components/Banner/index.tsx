5e33863bd7be7cb082a7c35cf4399e1a2559425a
Araviel <contact@araviel.io>
2022-06-09 22:09:08 +0200

    safe powered !

--- js/packages/web/src/components/Banner/index.tsx
+++ js/packages/web/src/components/Banner/index.tsx
@@ -55,7 +55,7 @@ export const Banner = (props: {
           {props.children}
           <div className="powered-by">
             <span>
-              POWERED BY <b>LEDAMINT</b>
+              POWERED BY <b>LEDAMINT</b> & <b>SAFECOIN</b>
             </span>
           </div>
         </div>
