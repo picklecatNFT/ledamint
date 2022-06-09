da72bacb4d4ac8ac5effbc3b844782c5ffd1ffbb
Araviel <contact@araviel.io>
2022-06-09 17:20:48 +0200

    how to buy steps

--- js/packages/web/src/components/HowToBuyModal/index.tsx
+++ js/packages/web/src/components/HowToBuyModal/index.tsx
@@ -20,17 +20,17 @@ export const HowToBuyModal: React.FC<HowToBuyModalProps> = ({
       cardProps={[
         {
           title: 'Create a SAFE wallet',
-          imgSrc: '/modals/how-to-buy-1.svg',
+          imgSrc: '/modals/how-to-buy-1.png',
           description: `SAFE is the cryptocurrency we use for purchases on ${LABELS.STORE_NAME}. To keep your SAFE safe, you’ll need a crypto wallet—we recommend using wallet.safecoin.org.`,
         },
         {
           title: 'Add funds to your wallet',
-          imgSrc: '/modals/how-to-buy-2.svg',
+          imgSrc: '/modals/how-to-buy-2.png',
           description: `To fund your wallet, you’ll need to purchase Safecoin. The easiest way is on SafeTrade. A new window will open where you can create an SafeTrade account and purchase SAFE.`,
         },
         {
           title: `Connect your wallet to ${LABELS.STORE_NAME}.`,
-          imgSrc: '/modals/how-to-buy-3.jpg',
+          imgSrc: '/modals/how-to-buy-3.png',
           description: `To connect your wallet, tap “Connect Wallet” here on the site. Select the Safecoin option, and your wallet will connect. After that, you can start bidding on NFTs.`,
           endElement: <ConnectButton className={'secondary-btn'} />,
         },
