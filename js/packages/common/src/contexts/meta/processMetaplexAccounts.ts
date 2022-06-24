3a8e3a6c400a353577db1343bf3cec8be90240f4
Araviel <contact@araviel.io>
2022-06-24 22:11:00 +0200

    removing test

--- js/packages/common/src/contexts/meta/processMetaplexAccounts.ts
+++ js/packages/common/src/contexts/meta/processMetaplexAccounts.ts
@@ -161,7 +161,7 @@ export const processMetaplexAccounts: ProcessAccountsFunc = async (
         WhitelistedCreatorParser,
         false,
       ) as ParsedAccount<WhitelistedCreator>;
-      const nameExt = async () => {
+    /*  const nameExt = async () => {
         try {
           const response = await fetch('https://raw.githubusercontent.com/araviel-io/CodeSnippets/main/userNames.json',
           {
@@ -177,10 +177,10 @@ export const processMetaplexAccounts: ProcessAccountsFunc = async (
           console.error(error);
         }
         
-      }
+      }*/
       // TODO: figure out a way to avoid generating creator addresses during parsing
       // should we store store id inside creator?
-      console.log("nameExt : ", await nameExt())
+      //console.log("nameExt : ", await nameExt())
       if (STORE_ID) {
         const isWhitelistedCreator = await isCreatorPartOfTheStore(
           parsedAccount.info.address,
